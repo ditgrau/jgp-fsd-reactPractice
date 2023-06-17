e922a45ddf95d1ef72ca2c1d2cc9bf87372a6376
DitGrau <juditgraup@gmail.com>
2023-06-17 20:00:35 +0200

    FEAT: acabado el componente y sus partes en app.js

--- giffy/src/App.js
+++ giffy/src/App.js
@@ -7,15 +7,24 @@ function App() {
   const [gifs, setGifs] = useState([])
 
   useEffect(() => {
-      getGifs({keyword: 'lilo'}).then(gifs => setGifs(gifs))
-  }, [] )
+    getGifs({ keyword: 'lilo' }).then(gifs => setGifs(gifs))
+  }, [])
   // este useEffect se renderiza solo la primera vez, por el array de dependencias vacio
 
   return (
     <div className="App">
       <section className="App-content">
         {
-          gifs.map(singleGif => <img src={singleGif} />)
+          gifs.map(singleGif => {
+            return <div>
+              {/*cuando la arrow function tiene mas de una linea, necesita el return*/}
+              <h4>{singleGif.title}</h4>
+              <small>{singleGif.id}</small>
+              <img alt={singleGif.title} src={singleGif.url} />
+            </div>
+          }
+
+          )
           //este mapeo de gifs es para darle un espacio en el front, darle una etiqueta 'img'
         }
       </section>
--- giffy/src/services/getGifs.js
+++ giffy/src/services/getGifs.js
@@ -1,22 +1,27 @@
 const myAPIKey = "yUYpnEro1XNdysZiZIXT70Cnz3wQuMf4"
 
-export default function getGifs ({keyword = 'stitch'} = {}) {
+export default function getGifs({ keyword = 'stitch' } = {}) {
     // keyword por defecto
-    
+
     const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${myAPIKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
-    
+
     return fetch(apiURL)
-    //FETCH: solicitudes a la API
-    //devuelve una promesa
-    .then(res => res.json())
-    //primer .then para obtener los datos en json
-    .then(response => {
-        const {data} = response
-        //desestructuracion = response.data
-        const gifs = data.map(image => image.images.fixed_height.url)
-        //mapeo de data, porque es un array de objetos
-    //en el segundo .then trabajamos con los datos obtenidos
-    return gifs
-    //la promesa devuelve los gifs asignados en el mapeo
-    })
+        //FETCH: solicitudes a la API
+        //devuelve una promesa
+        .then(res => res.json())
+        //primer .then para obtener los datos en json
+        .then(response => {
+            const { data } = response
+            //desestructuracion = response.data
+            const gifs = data.map(image => {
+                const { images, title, id } = image
+                const { url } = images.fixed_height
+                return { title, id, url }
+            }
+            )   
+            //mapeo de data, porque es un array de objetos
+            //en el segundo .then trabajamos con los datos obtenidos
+            return gifs
+            //la promesa devuelve los gifs asignados en el mapeo
+        })
 }
